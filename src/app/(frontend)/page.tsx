f213a8e70a8076c28378a72ceb6507ca57d175cf
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-09-16 13:16:18 +0200

    feat: implement posts listing with rich text content and images on homepage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
 import './styles.css'
+import { RichText } from '@payloadcms/richtext-lexical/react'
 
 export default async function HomePage() {
   const headers = await getHeaders()
@@ -15,45 +16,18 @@ export default async function HomePage() {
 
   const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
+  const { docs: posts } = await payload.find({ collection: 'posts' })
+
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
+    <div>
+      {user && <h1>Welcome back, {user.email}</h1>}
+
+      {posts.length > 0 ? <h1>Posts</h1> : <h1>No posts</h1>}
+      {posts?.map(post => <div key={post.id}>
+        <h2>{post.title}</h2>
+        <RichText data={post.content} />
+        <Image src={post.layout?.[0].image?.url} alt={post.layout?.[0].image?.alt} width={500} height={500} />
+      </div>)}
     </div>
   )
 }
