e3f43ba0bfeefb0fb98e5d0934fedb8c76399d79
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-09-16 11:12:58 +0200

    refactor: switch from MongoDB to SQLite database adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,5 @@
 // storage-adapter-import-placeholder
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
@@ -26,8 +26,10 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: mongooseAdapter({
-    url: process.env.DATABASE_URI || '',
+  db: sqliteAdapter({
+    client: {
+      url: process.env.DATABASE_URI || '',
+    },
   }),
   sharp,
   plugins: [
